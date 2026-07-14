119b8c33d9492e9d49530fa6586cad0b02065112
محمد أحمد محمد زيدان <95283264+Zedan2552000@users.noreply.github.com>
2026-07-14 15:35:54 +0300

    Initial commit: Add custom_components/egypt_trains/const.py

---
+++ custom_components/egypt_trains/const.py
@@ -0,0 +1,25 @@
+"""Constants for the Egypt Trains integration."""
+
+DOMAIN = "egypt_trains"
+CONF_DEPARTURE = "departure"
+CONF_ARRIVAL = "arrival"
+
+STATIONS = [
+    "Cairo",
+    "Alexandria",
+    "Tanta",
+    "Mansoura",
+    "Zagazig",
+    "Damietta",
+    "Ismailia",
+    "Port Said",
+    "Suez",
+    "Faiyum",
+    "Minya",
+    "Asyut",
+    "Sohag",
+    "Nagaa Hammadi",
+    "Luxor",
+    "Aswan",
+    "Marsa Matruh",
+]
